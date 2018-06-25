fa54e2fa8bd4159d2cdfbc3938c792a9edde5f07
minhthuy30197 <minhthuy30197@gmail.com>
2018-06-25 12:04:28 +0700

    Drum kit - js30 lesson 1

---
+++ javascript30/drum_kit/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', function(e) {
+	let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	if (audio) {
+		audio.currentTime = 0;
+		audio.play();
+		let thumb = document.querySelector(`.thumb[data-key="${e.keyCode}"]`);
+		thumb.classList.add('playing');
+	}
+});
+
+let keys = document.querySelectorAll('.thumb');
+keys.forEach(key => key.addEventListener("transitionend", function(e) {
+		if (e.propertyName == 'transform') {
+			key.classList.remove('playing');
+		}
+	})
+);
