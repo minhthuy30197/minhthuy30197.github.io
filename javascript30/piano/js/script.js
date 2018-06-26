d350bd2d738c49afce0375773e3ed4bbbae4c2e9
minhthuy30197 <minhthuy30197@gmail.com>
2018-06-26 09:49:05 +0700

    Change CSS Variables with JS in Piano

--- javascript30/piano/js/script.js
+++ javascript30/piano/js/script.js
@@ -14,3 +14,11 @@ keyboards.forEach(keyboard => keyboard.addEventListener('transitionend', functio
 		keyboard.classList.remove('pressed');
 	}
 }));
+
+document.getElementById('bgcolor').addEventListener('change', handleChange);
+document.getElementById('bgcolor').addEventListener('mousemove', handleChange);
+
+function handleChange() {
+	var unit = this.dataset.unit || '';
+	document.documentElement.style.setProperty(`--${this.name}`,`${this.value}${unit}`);
+}
