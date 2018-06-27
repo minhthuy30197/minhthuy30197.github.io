549b4c3728e7c559a3e4df8a3ec7e4b09b3b4b31
minhthuy30197 <minhthuy30197@gmail.com>
2018-06-27 09:36:13 +0700

    fix file json

--- javascript30/search_json/js/script.js
+++ javascript30/search_json/js/script.js
@@ -1,4 +1,4 @@
-const endpoint = "https://rawgit.com/minhthuy30197/minhthuy30197.github.io/master/javascript30/search_json/data.json";
+const endpoint = "https://cdn.rawgit.com/minhthuy30197/minhthuy30197.github.io/655000a1/javascript30/search_json/data.json";
 let infos = [];
 fetch(endpoint) 
 	.then(obj => obj.json())
