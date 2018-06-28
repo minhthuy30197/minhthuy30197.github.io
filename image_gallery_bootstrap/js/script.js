eced0a9907e03f8af1f37545bcc2942c0d66ffe7
minhthuy30197 <minhthuy30197@gmail.com>
2018-06-28 16:26:15 +0700

    Sticky nav

--- image_gallery_bootstrap/js/script.js
+++ image_gallery_bootstrap/js/script.js
@@ -1,7 +1,14 @@
 window.addEventListener('scroll', scrollPage);
 let thumbnails = document.querySelectorAll('.thumb');
+let nav = document.querySelector('nav');
 
 function scrollPage(e) {
+	if (window.scrollY > nav.offsetTop) {
+		document.body.classList.add('fixed-nav');
+	} else {
+		document.body.classList.remove('fixed-nav');
+	}
+
 	thumbnails.forEach(thumbnail => {
 		let halfImg = (window.scrollY + window.innerHeight) - thumbnail.clientHeight/2; 
 		let bottomImg = thumbnail.offsetTop + thumbnail.clientHeight;
