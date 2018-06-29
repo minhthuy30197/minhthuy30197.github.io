8389775f5feec65e1daf12d5c5744726d9429625
minhthuy30197 <minhthuy30197@gmail.com>
2018-06-29 10:54:42 +0700

    Stripe Follow Along Dropdown

--- javascript30/search_json/js/script.js
+++ javascript30/search_json/js/script.js
@@ -26,3 +26,36 @@ function displayInfo() {
 	}
 	suggestion.innerHTML = html;
 }
+
+let items = document.querySelectorAll('.drop-item');
+let nav = document.querySelector('.menu');
+let background = document.querySelector('.background');
+items.forEach(item => item.addEventListener('mouseover', overItem));
+items.forEach(item => item.addEventListener('mouseout', outItem));
+
+function overItem() {
+	this.classList.add('dropdown-display');
+	background.classList.add('bg-display');
+	setTimeout(() => {
+		if (this.classList.contains('dropdown-display')) {
+			this.classList.add('dropdown-appear');
+			let pos_item = this.querySelector('.dropdown').getBoundingClientRect();
+			let pos_nav = nav.getBoundingClientRect();
+			let pos_bg = {
+				width: pos_item.width,
+				height: pos_item.height,
+				top: pos_item.top - pos_nav.top,
+				left: pos_item.left - pos_nav.left
+			}
+			background.classList.add('bg-appear');
+			background.style.setProperty('width', `${pos_item.width}px`);
+			background.style.setProperty('height', `${pos_item.height}px`);
+			background.style.setProperty('transform', `translate(${pos_item.left}px, ${pos_item.top}px`);
+		}
+	}, 300);
+}
+
+function outItem() {
+	this.classList.remove('dropdown-display', 'dropdown-appear');
+	background.classList.remove('bg-display', 'bg-appear');
+}
