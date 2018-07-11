a1cfdc38be51480545a2d564323e0d99a90e5449
minhthuy30197 <minhthuy30197@gmail.com>
2018-07-11 07:39:13 +0700

    home page

--- icecream_shop/js/checkout.js
+++ icecream_shop/js/checkout.js
@@ -1,13 +1,13 @@
 $(function() { 
-	document.getElementById('subtotal').innerHTML = info_customer.subtotal;
-	document.getElementById('total').innerHTML = info_customer.total;
-	var order = document.querySelector('.part-order1 .left'); 
-	var cost = document.querySelector('.part-order1 .right'); 
+	document.getElementById('subtotal').innerHTML = info_customer.subtotal + " VND";
+	document.getElementById('total').innerHTML = info_customer.total + " VND";
+	var order = document.querySelector('.products .left'); 
+	var cost = document.querySelector('.products .right'); 
 	var leftHtml = '';
 	var rightHtml = '';
 	info_customer.cart.forEach(function(product) {
 		leftHtml += '<p>' + product.name + ' x ' + product.count + '</p>';
-		rightHtml += '<p>' +product.total+ '</p>';
+		rightHtml += '<p>' +product.total+ ' VND</p>';
 	});
 	order.innerHTML = leftHtml;
 	cost.innerHTML = rightHtml;
